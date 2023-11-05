93f27eee08698c29e2a6f3fcfe6fd97e915ae18e
千穂理 송화 <102968835+Songhwa-Rossana@users.noreply.github.com>
2023-11-05 21:16:47 +0800

    Add files via upload

---
+++ .sql
@@ -0,0 +1,21 @@
+-- CREATE TABLE tb_student(
+-- 	stu_num char(10) PRIMARY KEY,
+-- 	stu_name varchar(19) NOT NULL,
+-- 	sex varchar(10)	DEFAULT 'male' CHECK (sex IN ('male', 'female')),
+-- 	birthday datetime,
+-- 	dorm int
+-- );
+-- 
+-- 
+-- 
+-- ALTER TABLE tb_student
+-- MODIFY dorm varchar(10);
+-- desc tb_student;
+-- 
+
+RENAME TABLE tb_stu TO tb_student;
+
+ALTER TABLE tb_student
+DROP COLUMN contacts;
+
+desc tb_student;
